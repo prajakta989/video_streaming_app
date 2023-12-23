834fba474dc72fe1bf8a31b13af94296654edb10
Prajakta Naik <prajaktanaik98@gmail.com>
2023-12-23 11:04:56 +0530

    added validations to  sign in

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get("/add", (req, res) => {
+app.get("/", (req, res) => {
     res.send("Hello express users from backend")
 })
 
 const port = 5000;
-
 
 
 app.listen(port, () => {
